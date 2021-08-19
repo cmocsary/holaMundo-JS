3c2e9fdcb68dcd51e927b3d898c7f9fbfccb20a7
Carlos Mocsary <carlos.mocsary@gmail.com>
2021-08-19 00:38:09 -0300

    practica if

---
+++ js/estructurasCondicionales.js
@@ -0,0 +1,11 @@
+let numero1 = parseInt(prompt("Ingrese un numero"));
+let numero2 = parseInt(prompt("Ingrese otro numero"));
+
+let operacion = numero1 + numero2;
+console.log(operacion);
+
+if(numero1>numero2){
+    console.log("El numero "+ numero1 + " es mayor");
+}else{
+    console.log("El numero "+ numero2 + " es mayor");
+}
